513f3ff2dc0ea422b346560e96af6bc32b79d6a2
zaca100 <zaca10@gmail.com>
2022-03-26 10:49:40 -0300

    remove vulnerability

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
